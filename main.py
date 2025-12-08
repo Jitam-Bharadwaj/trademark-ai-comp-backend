ea11596b03b0616597f144bd69dcf04a1177e00f
jitam-Bharadwaj <jitam.desuntechnology@gmail.com>
2025-12-08 13:35:34 +0530

    feat: Add new allowed origin for CORS to support frontend deployment

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://trademark-ai-comp-frontend.vercel.app",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
         "http://localhost:3000",
